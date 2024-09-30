61bd5fd3b1cdcb11bfd4abb55aab4bde27cf5a77
Omar Ellaban <113574311+TheBruteforcer@users.noreply.github.com>
2024-09-30 14:31:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-from typing import Optional
+from fastapi import FastAPI, Query, Request, Depends
+from json import dumps, loads
+from sqlite3 import *
 
-from fastapi import FastAPI
+def get_db():
+    conn = connect("app.db")
+    try:
+        conn.cursor().execute("CREATE TABLE POSTS (ID TEXT, HTML TEXT)")
+    except:
+        pass
+    yield conn
 
 app = FastAPI()
 
+@app.get("/get-posts")
+def all_posts(conn = Depends(get_db)):
+    resp = []
+    ref = conn.cursor()
+    ref.execute("SELECT * FROM POSTS")
+    posts = ref.fetchall()
+    for post in posts :
+        resp.append({"id" : post[0], "parse" : post[1]})
+        
+    return resp
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/get-post")
+def post(id : str = Query(...), conn = Depends(get_db)):
+    resp = []
+    ref = conn.cursor()
+    ref.execute("SELECT * FROM POSTS WHERE ID = ?", (id,))
+    posts = ref.fetchall()
+    for post in posts :
+        resp.append({"id" : post[0], "parse" : post[1]})
+        
+    return resp
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/add-post")
+async def add(r : Request, conn = Depends(get_db)):
+   data = await r.json()
+   ref = conn.cursor()
+   ref.execute("SELECT * FROM POSTS")
+   id = 0
+   for p in ref.fetchall():
+       id += int(p[0])
+   ref.execute("INSERT INTO POSTS (?, ? )", (str(id), data["html"]))
+   return {"success" : True}
+   
+    
